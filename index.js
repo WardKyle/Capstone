c8c150651fc297e58a4a9616e4661291a2e55266
Kyle Ward <wardkyle77@gmail.com>
2023-10-24 17:50:31 -0500

    Add Sign In-API

--- index.js
+++ index.js
@@ -23,6 +23,51 @@ function mySort(a, b) {
   return a.platform.localeCompare(b.platform);
 }
 
+let formUsername, formPassword;
+function handleChange() {
+  let form = {};
+  form.username = formUsername.value;
+  form.password = formPassword.value;
+  save(form);
+}
+
+function save(param) {
+  const form = JSON.stringify(param);
+  window.localStorage.setItem("form", form);
+}
+
+let count = 0;
+function wait() {
+  const waitingText = document.querySelector("#signIn");
+  let waitTimeOut = setTimeout(() => {
+    count++;
+    waitingText.innerHTML += ".";
+    if (count === 6) {
+      waitingText.innerHTML = "Loading Users";
+      count = 0;
+    }
+    wait();
+  }, 500);
+  waitingText.innerHTML.includes("Loading User") ? waitTimeOut : clearID;
+  const clearID = clearTimeout(waitTimeOut);
+}
+
+function loaded() {
+  const axios = window.localStorage.getItem("axios");
+  const page = window.localStorage.getItem("page");
+  let timeoutID = setTimeout(() => {
+    loaded();
+  }, 500);
+
+  axios === "loaded" && page === "loaded" ? setButton() : timeoutID;
+
+  function setButton() {
+    const buttonText = document.querySelector("#signIn");
+    buttonText.innerHTML = `<div onClick="toggleSignIn()">Sign In</div>`;
+    clearTimeout(timeoutID);
+  }
+}
+
 function render(state = store.Home) {
   let renderThis = `${Header(state)} ${Main(state)}
   ${Footer()}`;
@@ -37,6 +82,23 @@ function render(state = store.Home) {
     utils.scrollDetect();
     utils.marketingAnim();
   }
+  afterRender(state);
+}
+function afterRender(page) {
+  if (page === store.Login) {
+    wait();
+
+    (function() {
+      window.localStorage.setItem("page", "loaded");
+      loaded();
+      formUsername = document.querySelector("#username");
+      formPassword = document.querySelector("#password");
+      let formItems = Array.from(document.querySelectorAll("input"));
+      formItems.forEach(el =>
+        el.addEventListener("input", handleChange, false)
+      );
+    })();
+  }
 }
 
 router.hooks({
@@ -47,6 +109,26 @@ router.hooks({
         : "Home";
     switch (view) {
       // const storedUserId = window.localStorage.getItem("user_id");
+      case "Login":
+        (async () => {
+          window.localStorage.setItem("axios", "still loading");
+          window.localStorage.setItem("page", "still loading");
+          await axiosCall();
+          window.localStorage.setItem("axios", "loaded");
+        })();
+        function axiosCall() {
+          axios
+            .get(`${process.env.PASSLOCKR_API_URL}/users`)
+            .then(response => {
+              storeUsers(response.data);
+              done();
+            })
+            .catch(error => {
+              console.log("Error occurred: ", error);
+              done();
+            });
+        }
+        break;
       case "Library":
         axios
           .get(
@@ -83,14 +165,6 @@ router
       window.localStorage.clear();
       hasAccess();
       render();
-      axios
-        .get(`${process.env.PASSLOCKR_API_URL}/users`)
-        .then(response => {
-          storeUsers(response.data);
-        })
-        .catch(error => {
-          console.log("Error occurred: ", error);
-        });
     },
     ":view": params => {
       let view = capitalize(params.data.view);
