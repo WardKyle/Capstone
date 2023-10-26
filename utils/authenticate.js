73d46457ddb3450c9c77e86d521f5499e3d77b86
Kyle Ward <wardkyle77@gmail.com>
2023-10-25 19:38:55 -0500

    Fix bug

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -1,18 +1,19 @@
 export default function authenticate(param = 1) {
+  let users;
   if (param != 1) {
-    const users = JSON.parse(window.localStorage.getItem("storedUsers"));
+    users = JSON.parse(window.localStorage.getItem("storedUsers"));
     return;
   }
   event.preventDefault();
 
-  const users = JSON.parse(window.localStorage.getItem("storedUsers"));
+  users = JSON.parse(window.localStorage.getItem("storedUsers"));
   const signIn_info = JSON.parse(window.localStorage.getItem("form"));
   const signIn_username = signIn_info.username;
   const signIn_password = signIn_info.password;
 
   let found, user_id;
 
-  found = users.find(
+  found = users.filter(
     el => el.username === signIn_username && el.password === signIn_password
   );
   if (found != undefined) {
