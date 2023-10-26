6231a1a78b8cc446ee6a2077ab4ae60a828eb4bb
Kyle Ward <wardkyle77@gmail.com>
2023-10-26 18:42:47 -0500

    Change authentication

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -1,45 +1,34 @@
-import { setTimeout } from "timers/promises";
+import * as store from "../store/index";
 
-export default function authenticate(param = 1) {
-  let users;
-  if (param != 1) {
-    users = JSON.parse(window.localStorage.getItem("storedUsers"));
-    return 1;
-  }
+export default function authenticate() {
   event.preventDefault();
-  (async () => {
-    try {
-      users = await JSON.parse(window.localStorage.getItem("storedUsers"));
-      const signIn_info = JSON.parse(window.localStorage.getItem("form"));
-      const signIn_username = signIn_info.username;
-      const signIn_password = signIn_info.password;
-      const usernameField = document.querySelector("input#username");
-      const passwordField = document.querySelector("input#password");
-      const invalid = document.querySelector("#invalid");
 
-      let found, user_id;
+  const users = store.Library.users;
+  const usernameField = document.querySelector("input#username");
+  const passwordField = document.querySelector("input#password");
+  const signIn_username = usernameField.value;
+  const signIn_password = passwordField.value;
+  const invalid = document.querySelector("#invalid");
 
-      if (users != null && users != undefined) {
-        found = users.find(
-          el =>
-            el.username === signIn_username && el.password === signIn_password
-        );
-        if (found != undefined) {
-          user_id = await found._id;
-          window.localStorage.setItem("user_id", user_id);
-          location.href = `${process.env.SERVER_LOCATION}/Library`;
-        } else {
-          usernameField.value = "";
-          passwordField.value = "";
-          invalid.innerHTML = "Invalid attempt, please try again";
-        }
-      } else {
-        usernameField.value = "";
-        passwordField.value = "";
-        invalid.innerHTML = "Invalid attempt, please try again";
-      }
-    } catch (error) {
-      console.error(error);
+  console.log(users);
+  let found, user_id;
+
+  if (users != null && users != undefined) {
+    found = users.find(
+      el => el.username === signIn_username && el.password === signIn_password
+    );
+    if (found != undefined) {
+      user_id = found._id;
+      window.localStorage.setItem("user_id", user_id);
+      location.href = `${process.env.SERVER_LOCATION}/Library`;
+    } else {
+      usernameField.value = "";
+      passwordField.value = "";
+      invalid.innerHTML = "Invalid attempt, please try again";
     }
-  })();
+  } else {
+    usernameField.value = "";
+    passwordField.value = "";
+    invalid.innerHTML = "Invalid attempt, please try again";
+  }
 }
