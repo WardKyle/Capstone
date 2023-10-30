2216153ca14a1b4094ec45df08cf18d368b681aa
Kyle Ward <wardkyle77@gmail.com>
2023-10-30 08:49:17 -0500

    Update styles

--- utils/onScrollEffect.js
+++ utils/onScrollEffect.js
@@ -1,18 +1,24 @@
 export function fadeInUp(element, param = window) {
   const containers = document.querySelectorAll(element);
+  const scrollVar = 50;
   param = param === window ? window : document.querySelector(param);
+  containers.forEach(el => {
+    if (el.offsetTop <= window.innerHeight) {
+      el.style.opacity = 1;
+    }
+  });
   param.addEventListener("scroll", function(event) {
     containers.forEach(function(el) {
       const thisScroll = el.offsetTop - window.innerHeight;
-      console.log(param.scrollTop);
       if (param === window) {
-        if (window.scrollY >= thisScroll && thisScroll >= 0) {
+        if (window.scrollY >= thisScroll + scrollVar && thisScroll >= 0) {
           el.classList.add("fade-in-up");
         } else {
           el.classList.remove("fade-in-up");
         }
       } else {
-        if (param.scrollTop >= thisScroll - 10 && thisScroll >= 0) {
+        console.log(el.offsetTop, window.innerHeight);
+        if (param.scrollTop >= thisScroll + scrollVar && thisScroll >= 0) {
           el.classList.add("fade-in-up");
         } else {
           el.classList.remove("fade-in-up");
